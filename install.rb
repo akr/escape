2710086fcbd77f9089a9b15d7c0f57099ec510af
Tanaka Akira <akr@fsij.org>
2007-11-08 15:28:29 +0000

    support --vendor option.
    git-svn-id: svn://svn.a-k-r.org/akr/escape/trunk@721 0ecacc03-ac1b-0410-941d-e442c79421a6

--- install.rb
+++ install.rb
@@ -10,9 +10,10 @@
 require 'optparse'
 require 'fileutils'
 
-def target_directory
+def target_directory(vendor)
+  pat = vendor ? %r{/vendor_ruby/[\d.]+\z} : %r{/site_ruby/[\d.]+\z}
   $:.each {|loc|
-    if %r{/site_ruby/[\d.]+\z} =~ loc
+    if pat =~ loc
       return loc
     end
   }
@@ -88,23 +89,25 @@ end
 
 $opt_n = false
 $opt_destdir = ""
+$opt_vendor = false
 ARGV.options {|q|
   q.banner = 'ruby install.rb [opts]'
   q.def_option('--help', 'show this message') {puts q; exit(0)}
   q.def_option('-n', "don't install") { $opt_n = true }
   q.def_option('--destdir=DESTDIR', "specify DESTDIR") {|destdir| $opt_destdir = destdir }
+  q.def_option('--vendor', 'install to vendor directory') { $opt_vendor = true }
   q.parse!
 }
 
 if $opt_n
-  dir = target_directory
+  dir = target_directory($opt_vendor)
   collect_target.each {|filename|
     puts "-> #{$opt_destdir}#{dir}/#{filename}"
   }
   exit
 else
   File.umask 022
-  dir = target_directory
+  dir = target_directory($opt_vendor)
   collect_target.each {|filename|
     install_file filename, "#{$opt_destdir}#{dir}/#{filename}"
   }
